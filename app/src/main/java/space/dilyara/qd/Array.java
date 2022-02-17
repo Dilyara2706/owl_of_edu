30b7beb56271cdfe1390d24bd89a1015af6bde48
DILYARA GABDULKHAKOVA <dilyara-gabdulkhakova@mail.ru>
2022-02-17 16:00:57 +0300

    Initial commit

---
+++ app/src/main/java/space/dilyara/qd/Array.java
@@ -0,0 +1,59 @@
+package space.dilyara.qd;
+
+public class Array {
+
+    final int[] images1= {
+            R.drawable.one,
+            R.drawable.two,
+            R.drawable.tri,
+            R.drawable.four,
+            R.drawable.five,
+            R.drawable.six,
+            R.drawable.seven,
+            R.drawable.eight,
+            R.drawable.nine
+    };
+
+
+    final int[] text1 = {
+            R.string.lvl1text1,
+            R.string.lvl1text2,
+            R.string.lvl1text3,
+            R.string.lvl1text4,
+            R.string.lvl1text5,
+            R.string.lvl1text6,
+            R.string.lvl1text7,
+            R.string.lvl1text8,
+            R.string.lvl1text9
+
+    };
+//массив для 2 ур
+    final int[] images2= {
+            R.drawable.level2_one,
+            R.drawable.level2_two,
+            R.drawable.level2_three,
+            R.drawable.level2_four,
+            R.drawable.level2_five,
+            R.drawable.level2_six,
+            R.drawable.level2_seven,
+            R.drawable.level2_eight,
+            R.drawable.level2_nine,
+            R.drawable.level2_ten
+    };
+
+
+    final int[] text2 = {
+            R.string.lvl2text1,
+            R.string.lvl2text2,
+            R.string.lvl2text3,
+            R.string.lvl2text4,
+            R.string.lvl2text5,
+            R.string.lvl2text6,
+            R.string.lvl2text7,
+            R.string.lvl2text8,
+            R.string.lvl2text9,
+            R.string.lvl2text10
+
+    };
+
+}
